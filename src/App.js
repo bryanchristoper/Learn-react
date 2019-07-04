10d6a8d75b758207728daceac4c422242a6a69fa
bryanchristoper <bryandes4@gmail.com>
2019-07-04 20:39:20 +0700

    Add redirects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList'
 import Chat from './pages/Chat'
@@ -7,11 +7,11 @@ import Chat from './pages/Chat'
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Route path="/" exact component={Home} />
         <Route path="/products" component={ProductList} />
         <Route path="/chat" component={Chat} />
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
