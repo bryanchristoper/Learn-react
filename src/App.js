4d104b0fa873b81aba29b4f89bfb71e20c467ba5
bryanchristoper <bryandes4@gmail.com>
2019-07-04 20:42:56 +0700

    Fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList'
 import Chat from './pages/Chat'
@@ -7,11 +7,11 @@ import Chat from './pages/Chat'
 class App extends React.Component {
   render() {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <Route path="/" exact component={Home} />
         <Route path="/products" component={ProductList} />
         <Route path="/chat" component={Chat} />
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
